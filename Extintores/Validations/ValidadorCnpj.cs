9c3a1ec4952aeff3b0247f29dd64dd80db631f00
LuckyBressan <lucas.a.dos.s.bressan@gmail.com>
2025-04-20 22:31:04 -0300

    feat: criando validadores de dados do cliente

---
+++ Extintores/Validations/ValidadorCnpj.cs
@@ -0,0 +1,45 @@
+﻿namespace Extintores.Validations
+{
+    public static class ValidadorCNPJ
+    {
+        public static bool Validar(string cnpj)
+        {
+            if (string.IsNullOrWhiteSpace(cnpj))
+                return false;
+
+            // Remove qualquer caractere que não seja número
+            cnpj = new string(cnpj.Where(char.IsDigit).ToArray());
+
+            if (cnpj.Length != 14)
+                return false;
+
+            // Elimina CNPJs com todos os dígitos iguais
+            if (cnpj.Distinct().Count() == 1)
+                return false;
+
+            int[] multiplicador1 = { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            var tempCnpj = cnpj.Substring(0, 12);
+            var soma = 0;
+
+            for (int i = 0; i < 12; i++)
+                soma += (tempCnpj[i] - '0') * multiplicador1[i];
+
+            int resto = (soma % 11);
+            int digito1 = resto < 2 ? 0 : 11 - resto;
+
+            tempCnpj += digito1;
+            soma = 0;
+
+            for (int i = 0; i < 13; i++)
+                soma += (tempCnpj[i] - '0') * multiplicador2[i];
+
+            resto = (soma % 11);
+            int digito2 = resto < 2 ? 0 : 11 - resto;
+
+            return cnpj.EndsWith($"{digito1}{digito2}");
+        }
+    }
+
+}
---
+++ Extintores/Validations/ValidadorCpf.cs
@@ -0,0 +1,41 @@
+﻿namespace Extintores.Validations
+{
+    public static class ValidadorCPF
+    {
+        public static bool Validar(string cpf)
+        {
+            if (string.IsNullOrWhiteSpace(cpf))
+                return false;
+
+            // Remove caracteres não numéricos
+            cpf = new string(cpf.Where(char.IsDigit).ToArray());
+
+            if (cpf.Length != 11)
+                return false;
+
+            // Elimina CPFs com todos os dígitos iguais
+            if (cpf.Distinct().Count() == 1)
+                return false;
+
+            // Cálculo do primeiro dígito verificador
+            var soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += (cpf[i] - '0') * (10 - i);
+
+            int digito1 = soma % 11;
+            digito1 = digito1 < 2 ? 0 : 11 - digito1;
+
+            // Cálculo do segundo dígito verificador
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += (cpf[i] - '0') * (11 - i);
+
+            int digito2 = soma % 11;
+            digito2 = digito2 < 2 ? 0 : 11 - digito2;
+
+            // Verifica se os dígitos batem
+            return cpf[9] - '0' == digito1 && cpf[10] - '0' == digito2;
+        }
+    }
+
+}
